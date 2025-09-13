ec167f9a9e8eb0a400b591945bb5e206685bc35c
chillyxs1qq <joacabarreiro10@gmail.com>
2025-09-13 09:31:26 -0300

    nashe

--- Sistema de Gestion de Biblioteca/test.py
+++ Sistema de Gestion de Biblioteca/test.py
@@ -10,8 +10,12 @@ ventana.configure(bg="#f0f0f0")
 ventana.iconbitmap(r"C:\Users\joaca\OneDrive\Desktop\CERP\Segundo Semestre\Programacion II\PROYECTO FINAL\icono tun tun sahur\tun_tun_sahur.ico")
 
 # Formulario con grid
+# ID del libro (automático, no editable)
 tk.Label(ventana, text="ID del Libro:", font=("Arial", 12, "bold"), bg="#f0f0f0").grid(row=0, column=0, padx=10, pady=5, sticky="e")
-tk.Entry(ventana, width=30).grid(row=0, column=1, padx=10, pady=5)
+entrada_id = tk.Entry(ventana, width=5, font=("Arial", 12, "bold"), justify="center")  # más cuadrado y centrado
+entrada_id.insert(0, "1")  # por defecto empieza en 1
+entrada_id.config(state="readonly", readonlybackground="#d9edf7", fg="#31708f", bd=2)  # solo lectura con color
+entrada_id.grid(row=0, column=1, padx=10, pady=5, sticky="w")
 
 tk.Label(ventana, text="Título:", font=("Arial", 12, "bold"), bg="#f0f0f0").grid(row=1, column=0, padx=10, pady=5, sticky="e")
 tk.Entry(ventana, width=30).grid(row=1, column=1, padx=10, pady=5)
