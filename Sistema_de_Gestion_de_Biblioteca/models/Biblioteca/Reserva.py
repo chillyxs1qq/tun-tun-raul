7976d8f352490a3aaed90f70ca8b34dbe89085cd
Dancl3 <joseenriquegonzalezchamorro@gmail.com>
2025-09-14 21:45:19 -0300

    Actualizacion

---
+++ Sistema_de_Gestion_de_Biblioteca/models/Biblioteca/Reserva.py
@@ -0,0 +1,13 @@
+class Reserva:
+    def __init__(self, idmaterial):
+        self.__idmaterial = idmaterial
+        self.__colaSocios = []
+
+
+    def encolar(self, idSocio):
+        self.__colaSocios.append(idSocio)
+
+    def desencolar(self):
+        self.__colaSocios.pop()
+
+    def siguiente(self):
